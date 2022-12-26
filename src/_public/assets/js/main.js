9e7b7e0b13e6fc0501bd939e86f8a075d2e2364d
nguyendev2018 <nguyendev2018@gmail.com>
2022-12-26 12:06:46 -0800

    tre

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,7 +1,41 @@
-$("#hide").click(function(){
-    $("p").hide();
-  });
-  
-  $("#show").click(function(){
-    $("p").show();
-  });
+$(window).on('resize', function() {
+
+    if ($(window).width() <=900) {
+        $(".c-point__list").slick({
+        
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+            responsive: [
+                {
+                  breakpoint: 700,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 2,
+                  }
+                }
+            ]
+          });
+    }
+    if($(window).width() <=768) {
+      $(".c-col__bottom").slideUp();
+$(".c-col__button").click(function (e) {
+    $(this).prev().slideToggle(); 
+});
+    }
+})
+
+$(".c-point__itembutton").click(function(){
+ $(".c-point__itemlayer").toggleClass("is-active");
+})
+
+$(".c-headersp__toggle").click(function() {
+  $(".c-headersp__bg").toggleClass("is-active");
+  $(".c-headersp__close").toggleClass("is-active");
+  if ($(".c-headersp__bg").hasClass("is-active")) {
+    $("body").addClass("overflow");
+} else {
+    $("body").removeClass("overflow");
+}
+})
